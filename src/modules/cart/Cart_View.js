e4e0ec793f82130b8518a555e3a8c5aee31db9f9
arifzamri <arifdroiddeveloper@gmail.com>
2020-11-04 12:17:10 +0800

    add checkbox delivery location to order

--- src/modules/cart/Cart_View.js
+++ src/modules/cart/Cart_View.js
@@ -7,6 +7,8 @@ import { UserData_Context } from '../../context-provider/UserContext';
 import { URL, URL_DEV_2, URL_google_bucket } from "@env"
 import Axios from 'axios';
 import { Alert } from 'react-native';
+import CheckBox from 'react-native-check-box';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 
@@ -25,6 +27,13 @@ const sample_data = [
 const Cart_View = () => {
 
     const [refToken_context, setRefToken_context, currentUser, setCurrentUser, currentTenant, setCurrentTenant, cartData, setCartData] = useContext(UserData_Context)
+
+    const [toggleLocationDelivery_1, setToggleLocationDelivery_1] = useState(false)
+    const [toggleLocationDelivery_2, setToggleLocationDelivery_2] = useState(false)
+    const [toggleLocationDelivery_3, setToggleLocationDelivery_3] = useState(false)
+
+    const [toggleCODTime_1, setToggleCODTime_1] = useState(false)
+    const [toggleCODTime_2, setToggleCODTime_2] = useState(false)
 
     const _removeThisItem = () => {
         setCartData(prevData => {
@@ -48,17 +57,65 @@ const Cart_View = () => {
         })
     }
 
-    console.log('\n\n cart order', cartData)
+    // console.log('\n\n cart order', cartData)
 
-    const __pressOrder = async() => {
+    useEffect(()=>{
+
+        if(toggleLocationDelivery_1){             
+            setToggleLocationDelivery_2(false)
+            setToggleLocationDelivery_3(false)
+        }
+
+
+    },[toggleLocationDelivery_1])
+
+    useEffect(()=>{
+
+
+        if(toggleLocationDelivery_2){ 
+            setToggleLocationDelivery_1(false)
+            setToggleLocationDelivery_3(false)
+        }
+
+
+    },[ toggleLocationDelivery_2])
+
+    useEffect(()=>{
+
+        if(toggleLocationDelivery_3){ 
+            setToggleLocationDelivery_1(false)
+            setToggleLocationDelivery_2(false)
+        }
+
+    },[ toggleLocationDelivery_3])
+
+    useEffect(()=>{
+
+        if(toggleCODTime_1){ 
+            setToggleCODTime_2(false)
+            
+        }
+
+    },[ toggleCODTime_1])
+
+    useEffect(()=>{
+
+        if(toggleCODTime_2){ 
+            setToggleCODTime_1(false)
+            
+        }
+
+    },[ toggleCODTime_2])
+
+    const __pressOrder = async () => {
 
 
         let cart_now = cartData;
 
-        let cart_array_to_order = cart_now.map(el=>{
+        let cart_array_to_order = cart_now.map(el => {
             return {
-                productId:el.id,
-                quantity_ordered:parseInt(el.quantity),
+                productId: el.id,
+                quantity_ordered: parseInt(el.quantity),
                 total_price: parseFloat(el.product_price) * parseInt(el.quantity)
             }
         })
@@ -77,115 +134,172 @@ const Cart_View = () => {
 
         try {
 
-        let resp_create_order = await Axios.post(`${URL}/api/tenant/${currentTenant}/orders`, { data }, config);
+            let resp_create_order = await Axios.post(`${URL}/api/tenant/${currentTenant}/orders`, { data }, config);
 
-        if(resp_create_order) {
-            Alert.alert('order success')
-            setCartData(null)
-        }   
-            
+            if (resp_create_order) {
+                Alert.alert('order success')
+                setCartData(null)
+            }
+
         } catch (error) {
             Alert.alert('order fail')
         }
-        
+
 
     }
 
     return (
         <SafeAreaView style={{ flex: 1, }}>
-            <Text style={{ marginLeft: 20, fontSize: 30, marginTop: 25, }}>My Cart</Text>
+            <ScrollView>
+                <Text style={{ marginLeft: 20, fontSize: 30, marginTop: 25, }}>My Cart</Text>
 
-            <TouchableOpacity style={{ alignSelf: 'flex-end', marginRight: 30, marginHorizontal: 10, marginBottom: 20 }} onPress={() => _removeThisItem()}>
-                <Image source={image_delete} resizeMode='contain' style={{ width: 15, alignSelf: 'center', height: 15 }}></Image>
+                <TouchableOpacity style={{ alignSelf: 'flex-end', marginRight: 30, marginHorizontal: 10, marginBottom: 20 }} onPress={() => _removeThisItem()}>
+                    <Image source={image_delete} resizeMode='contain' style={{ width: 15, alignSelf: 'center', height: 15 }}></Image>
 
-            </TouchableOpacity>
+                </TouchableOpacity>
 
-            <FlatList
+                <FlatList
 
-                data={cartData}
+                    data={cartData}
 
-                renderItem={({ item, index }) => {
-                    return (
+                    renderItem={({ item, index }) => {
+                        return (
 
-                        <View style={{
-                            // flexDirection:'row', 
-                            justifyContent: 'center',
-                            alignSelf: 'center',
-                            flex: 1,
-                            width: '98%'
-                            // height: 295
-                        }}>
-                            <CardView
-                                style={{ marginVertical: 7, backgroundColor: 'white', flex: 1, marginHorizontal: 10 }}
-                                cardElevation={2}
-                                cardMaxElevation={2}
-                                cornerRadius={30}
-                            >
-                                <View
-                                    style={{
-                                        justifyContent: 'center', margin: 10,
-                                        marginTop: 25, flexDirection: 'row', flex: 1
-                                    }}
-
+                            <View style={{
+                                // flexDirection:'row', 
+                                justifyContent: 'center',
+                                alignSelf: 'center',
+                                flex: 1,
+                                width: '98%'
+                                // height: 295
+                            }}>
+                                <CardView
+                                    style={{ marginVertical: 7, backgroundColor: 'white', flex: 1, marginHorizontal: 10 }}
+                                    cardElevation={2}
+                                    cardMaxElevation={2}
+                                    cornerRadius={30}
                                 >
-                                    <View style={{ flex: 1 }}>
-                                        <Image source={{ uri: "data:image/png;base64," + item.image }} resizeMode='contain' style={{ width: 75, alignSelf: 'center', height: 75 }}></Image>
-                                        {/* <Text style={{ marginTop: 10, marginLeft: 8, fontSize: 15, color: 'black', fontWeight: '500', }}>
+                                    <View
+                                        style={{
+                                            justifyContent: 'center', margin: 10,
+                                            marginTop: 25, flexDirection: 'row', flex: 1
+                                        }}
+
+                                    >
+                                        <View style={{ flex: 1 }}>
+                                            <Image source={{ uri: "data:image/png;base64," + item.image }} resizeMode='contain' style={{ width: 75, alignSelf: 'center', height: 75 }}></Image>
+                                            {/* <Text style={{ marginTop: 10, marginLeft: 8, fontSize: 15, color: 'black', fontWeight: '500', }}>
                                             {item.product_name}
                                         </Text> */}
-                                        {/* <Text style={{ fontSize: 13, color: 'gray', marginLeft: 8, marginTop: 4 }}>
+                                            {/* <Text style={{ fontSize: 13, color: 'gray', marginLeft: 8, marginTop: 4 }}>
                                             {item.product_description}
                                         </Text> */}
-                                        <View style={{ flexDirection: 'row', flex: 1, width: 80, borderWidth: 0.5, justifyContent: 'center', margin: 15, alignSelf: 'center', }}>
-                                            <TouchableOpacity style={{ flex: 1, alignSelf: 'center' }}><Text style={{ alignSelf: 'center', }}>-</Text></TouchableOpacity>
-                                            <View style={{ flex: 1, borderLeftWidth: 0.5, borderRightWidth: 0.5 }}><Text style={{ alignSelf: 'center', }}>{item.quantity}</Text></View>
-                                            <TouchableOpacity style={{ flex: 1, alignSelf: 'center', }}><Text style={{ alignSelf: 'center', }}>+</Text></TouchableOpacity>
+                                            <View style={{ flexDirection: 'row', flex: 1, width: 80, borderWidth: 0.5, justifyContent: 'center', margin: 15, alignSelf: 'center', }}>
+                                                <TouchableOpacity style={{ flex: 1, alignSelf: 'center' }}><Text style={{ alignSelf: 'center', }}>-</Text></TouchableOpacity>
+                                                <View style={{ flex: 1, borderLeftWidth: 0.5, borderRightWidth: 0.5 }}><Text style={{ alignSelf: 'center', }}>{item.quantity}</Text></View>
+                                                <TouchableOpacity style={{ flex: 1, alignSelf: 'center', }}><Text style={{ alignSelf: 'center', }}>+</Text></TouchableOpacity>
+                                            </View>
+
                                         </View>
+                                        <View style={{ flex: 2 }}>
 
-                                    </View>
-                                    <View style={{ flex: 2 }}>
+                                            <Text style={{ marginLeft: 8, fontSize: 15, color: 'black', fontWeight: '500', }}>
+                                                {item.product_name}
+                                            </Text>
+                                            <Text style={{ fontSize: 13, color: 'gray', marginLeft: 8, marginTop: 4 }}>
+                                                {item.product_description}
+                                            </Text>
 
-                                        <Text style={{ marginLeft: 8, fontSize: 15, color: 'black', fontWeight: '500', }}>
-                                            {item.product_name}
-                                        </Text>
-                                        <Text style={{ fontSize: 13, color: 'gray', marginLeft: 8, marginTop: 4 }}>
-                                            {item.product_description}
-                                        </Text>
-
-                                    </View>
-                                    {/* <TouchableOpacity style={{flex:0.3 }} onPress={()=>_removeThisItem(item)}>
+                                        </View>
+                                        {/* <TouchableOpacity style={{flex:0.3 }} onPress={()=>_removeThisItem(item)}>
                                         <Image source={image_delete} resizeMode='contain' style={{ width: 15, alignSelf: 'center', height: 15 }}></Image>
                                        
                                     </TouchableOpacity> */}
 
 
-                                </View>
+                                    </View>
 
-                            </CardView>
-
-
-                        </View>
-
-                    )
-                }}>
+                                </CardView>
 
 
-            </FlatList>
+                            </View>
 
-            {/* <View style={{width:'80%', alignSelf:'center', marginHorizontal:20, marginBottom:20, borderRadius:20}}> */}
-            <LinearGradient colors={['#FEC140', '#FC986E', '#FA709A']} style={styles.linearGradient_2}
-                start={{ x: 0.0, y: 0.25 }} end={{ x: 0.5, y: 1.0 }}
-            // locations={[0,0.4,0.85]}
-            // locations={[0.2,0.5,0.85]}
-            >
-                <TouchableOpacity style={{ height: 50, width: 200, alignSelf: 'center', justifyContent: 'center', borderRadius: 20 }} onPress={__pressOrder}>
+                        )
+                    }}>
 
-                    <Text style={{ alignSelf: 'center', color: 'white' }}>Order Now</Text>
 
-                </TouchableOpacity>
-            </LinearGradient>
-            {/* </View> */}
+                </FlatList>
 
+                <Text style={{ marginLeft: 20, fontSize: 16, marginTop: 35, marginBottom: 15 }}>Select Preferred COD Location</Text>
+
+                <CheckBox
+                    style={{ width: 230, marginLeft: 20, marginTop: 5, marginBottom: 10 }}
+                    onClick={() => {
+                        setToggleLocationDelivery_1(!toggleLocationDelivery_1)
+                    }}
+                    uncheckedCheckBoxColor={'gray'}
+                    checkedCheckBoxColor={'red'}
+                    isChecked={toggleLocationDelivery_1}
+                    rightText={"In front of hostel delima"}
+                />
+                <CheckBox
+                    style={{ width: 230, marginLeft: 20, marginTop: 5, marginBottom: 10 }}
+                    onClick={() => {
+                        setToggleLocationDelivery_2(!toggleLocationDelivery_2)
+                    }}
+                    uncheckedCheckBoxColor={'gray'}
+                    checkedCheckBoxColor={'red'}
+                    isChecked={toggleLocationDelivery_2}
+                    rightText={"In front of hostel zamrud"}
+                />
+                <CheckBox
+                    style={{ width: 230, marginLeft: 20, marginTop: 5, marginBottom: 30 }}
+                    onClick={() => {
+                        setToggleLocationDelivery_3(!toggleLocationDelivery_3)
+                    }}
+                    uncheckedCheckBoxColor={'gray'}
+                    checkedCheckBoxColor={'red'}
+                    isChecked={toggleLocationDelivery_3}
+                    rightText={"At the cafe"}
+                />
+
+                <Text style={{ marginLeft: 20, fontSize: 16, marginTop: 15, marginBottom: 15 }}>Select Preferred COD Time</Text>
+
+                <CheckBox
+                    style={{ width: 230, marginLeft: 20, marginTop: 5, marginBottom: 10 }}
+                    onClick={() => {
+                        setToggleCODTime_1(!toggleCODTime_1)
+                    }}
+                    uncheckedCheckBoxColor={'gray'}
+                    checkedCheckBoxColor={'red'}
+                    isChecked={toggleCODTime_1}
+                    rightText={"Tuesday  5pm"}
+                />
+                <CheckBox
+                    style={{ width: 230, marginLeft: 20, marginTop: 5, marginBottom: 40 }}
+                    onClick={() => {
+                        setToggleCODTime_2(!toggleCODTime_2)
+                    }}
+                    uncheckedCheckBoxColor={'gray'}
+                    checkedCheckBoxColor={'red'}
+                    isChecked={toggleCODTime_2}
+                    rightText={"Friday     5pm"}
+                />
+               
+                {/* <View style={{width:'80%', alignSelf:'center', marginHorizontal:20, marginBottom:20, borderRadius:20}}> */}
+                <LinearGradient colors={['#FEC140', '#FC986E', '#FA709A']} style={styles.linearGradient_2}
+                    start={{ x: 0.0, y: 0.25 }} end={{ x: 0.5, y: 1.0 }}
+                // locations={[0,0.4,0.85]}
+                // locations={[0.2,0.5,0.85]}
+                >
+                    <TouchableOpacity style={{ height: 50, width: 200, alignSelf: 'center', justifyContent: 'center', borderRadius: 20 }} onPress={__pressOrder}>
+
+                        <Text style={{ alignSelf: 'center', color: 'white' }}>Order Now</Text>
+
+                    </TouchableOpacity>
+                </LinearGradient>
+                {/* </View> */}
+            </ScrollView>
         </SafeAreaView>
     )
 }
